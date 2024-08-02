905514c95c0a6b264a3bbf61afc256864e07d6f7
Anjana <anjanapcollege@gmail.com>
2024-08-02 17:08:17 +0530

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Anjana Rajamani on 8/2/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
