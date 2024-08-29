5bd86f1654c56d4de69c6546d0e1440895a6633b
Anjana <anjanapcollege@gmail.com>
2024-08-29 11:36:57 +0530

    Added a custom tip range, a field to reflect the total amount with tip, and a mechanism to hide the TextField keypad after entering the bill amount.

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -11,15 +11,32 @@ struct ContentView: View {
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
     let tipPercentages = [10, 15, 20, 25]
+    var totalWithTip: Double {
+        let tip = Double(tipPercentage) / 100 * checkAmount
+        let total = checkAmount + tip
+        return total
+    }
+    var totalPerPerson: Double {
+        // calculate total per person here
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        return amountPerPerson
+    }
     var body: some View {
         NavigationStack {
             Form {
-                Section {
+                Section("Bill Amount") {
                     TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                         .keyboardType(.decimalPad)
-                    
-                    Picker("Number of people", selection: $numberOfPeople) {
+                        .focused($amountIsFocused)
+                }
+                Section {
+                    Picker("number of people", selection: $numberOfPeople) {
                         ForEach(2..<100) {
                             Text("\($0) people")
                         }
@@ -27,18 +44,35 @@ struct ContentView: View {
                     .pickerStyle(.navigationLink)
                 }
                 Section("How much tip do you want to leave?") {
-                    Picker("Tip percentage", selection: $tipPercentage) {
-                        ForEach(tipPercentages, id: \.self) {
-                            Text($0, format: .percent)
+                    VStack {
+                        Picker("Tip percentage", selection: $tipPercentage) {
+                            ForEach(0..<101, id: \.self) {
+                                Text($0, format: .percent)
+                            }
                         }
+                        .pickerStyle(.segmented)
+                        .padding(.top, 5)
+                        
+                        Text("Tip Amount: \(tipPercentage)")
+                            .padding(.top, 5)
+                            .frame(maxWidth: .infinity, alignment: .leading)
                     }
-                    .pickerStyle(.segmented)
                 }
-                Section {
-                    Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                Section("Total amount with tip") {
+                    Text(totalWithTip, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+                Section("Amount per person") {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                 }
             }
-            .navigationTitle("WeSplit")
+            .toolbar {
+                if amountIsFocused {
+                    Button("Done") {
+                            amountIsFocused = false
+                    }
+                }
+            }
+            .navigationTitle("split the bill")
         }
     }
 }
